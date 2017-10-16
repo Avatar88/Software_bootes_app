f3a067b5f1dd94891832a80712fd9684f5d3a38d
jpac <jpac.ultimate88@gmail.com>
2017-10-16 00:27:14 -0500

    aca se deja tal cual la rama sin el list view

--- Components/screen3.js
+++ Components/screen3.js
@@ -2,21 +2,7 @@ import React, {Component} from 'react';
 import{View, Text, Alert} from 'react-native';
 import { FormLabel, FormInput,FormValidationMessage,Button,List, ListItem } from 'react-native-elements';
 
-const list = [
-  {
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President',
-    icon: 'av-timer'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman',
-     icon: 'flight-takeoff'
-  }
 
-]
 class Screen3 extends Component {
 constructor(props){
   super(props);
@@ -56,19 +42,7 @@ constructor(props){
   render() {
     return(
       <View>
-      <List containerStyle={{marginBottom: 20}}>
-      {
-        list.map((l, i) => (
-          <ListItem
-          roundAvatar
-          avatar={{uri:l.avatar_url}}
-          key={i}
-          title={l.name}
-          rightIcon={{name: l.  icon}}
-          />
-        ))
-      }
-      </List>
+
       <FormInput
       placeholder = "Cedula"
       onChangeText={(cedula) => this.setState({cedula})}
